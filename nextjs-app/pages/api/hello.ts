81132e534e8f67e3d7632cfdc0195889f3a36beb
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2023-03-07 16:33:59 -0500

    made front end request all the users from backend for a test

--- nextjs-app/pages/api/hello.ts
+++ nextjs-app/pages/api/hello.ts
@@ -1,13 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient, User } from "@prisma/client";
 
 type Data = {
-  name: string;
+  users: User[];
 };
 
-export default function handler(
+export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const prisma = new PrismaClient();
+
+  if(!prisma) {
+    console.log("noo");
+  }
+
+  const users = await prisma.user.findMany();
+  res.status(200).json({ users: users });
 }
