bf7d540bdd497ffca95c3cc5768dbd030c55fef1
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2023-03-11 18:01:35 -0500

    added small styling to index and me pages

--- nextjs-app/pages/me.tsx
+++ nextjs-app/pages/me.tsx
@@ -1,20 +1,51 @@
 import { signIn, signOut, useSession } from "next-auth/react";
+import styles from "@/styles/Me.module.css";
+import { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Me() {
-  const { data } = useSession();
+  const { data: session } = useSession();
+  const [name, setName] = useState("");
+  const router = useRouter();
 
-  if (data) {
-    return (
-      <>
-        Signed in as {JSON.stringify(data)} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
+  const onSubmit = async () => {
+    if (session) {
+      await axios
+        .put("/api/users", {
+          ...session.user,
+          name: name,
+        });
+        await axios.get("/api/auth/session?update");
+
+      router.reload();
+    }
+  };
+
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </>
+    <div className={styles.me}>
+      {(session && (
+        <>
+          {/* update info here */}
+          Signed in as {session.user.name ?? "null"} <br />
+          <label>Enter name</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(text) => setName(text.target.value)}
+          />
+          <button onClick={onSubmit}>Submit</button>
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )) || (
+        <>
+          Not signed in <br />
+          <button onClick={() => signIn()}>Sign in</button>
+        </>
+      )}
+
+      <Link href="/">Go back</Link>
+    </div>
   );
 }
